f01b902b49042039e62da1137ebc16f5ac20e771
irbster01 <irbster@gmail.com>
2026-03-05 07:59:31 -0600

    Committing 28 items from workspace 4241c9c7-5818-4c56-9220-faf632741770

--- servicepoint/servicepoint_warehouse.Warehouse/dbo/Tables/sp_entry_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE [dbo].[sp_entry_table] (
-
-	[Client ID] bigint NULL, 
-	[Entry Date] date NULL, 
-	[Entry Exit ID] bigint NULL, 
-	[Exit Date] date NULL, 
-	[Provider] varchar(8000) NULL
-);
